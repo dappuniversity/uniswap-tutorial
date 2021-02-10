e850d5e92853c84f971d13f06e8447bb271708b9
anton-karlovskiy-g <antonkarlovskiy@outlook.com>
2021-02-10 12:12:38 +0300

    [chore] clean up

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,16 @@
-import React, { useEffect } from 'react'
-import './App.css'
-import { ApolloClient } from 'apollo-client'
-import { InMemoryCache } from 'apollo-cache-inmemory'
-import { HttpLink } from 'apollo-link-http'
-import { useQuery } from '@apollo/react-hooks'
-import gql from 'graphql-tag'
-import uniswapLogo from '../uniswap-logo.png'
-import daiLogo from '../dai-logo.png'
 
-export const client = new ApolloClient({
+import React from 'react';
+import { ApolloClient } from 'apollo-client';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { HttpLink } from 'apollo-link-http';
+import { useQuery } from '@apollo/react-hooks';
+import gql from 'graphql-tag';
+
+import uniswapLogo from '../assets/uniswap-logo.png';
+import daiLogo from '../assets/dai-logo.png';
+import './App.css';
+
+const client = new ApolloClient({
   link: new HttpLink({
     uri: 'https://api.thegraph.com/subgraphs/name/uniswap/uniswap-v2'
   }),
@@ -16,7 +18,7 @@ export const client = new ApolloClient({
     mode: 'no-cors'
   },
   cache: new InMemoryCache()
-})
+});
 
 const DAI_QUERY = gql`
   query tokens($tokenAddress: Bytes!) {
@@ -25,7 +27,7 @@ const DAI_QUERY = gql`
       totalLiquidity
     }
   }
-`
+`;
 
 const ETH_PRICE_QUERY = gql`
   query bundles {
@@ -33,19 +35,27 @@ const ETH_PRICE_QUERY = gql`
       ethPrice
     }
   }
-`
+`;
 
 function App() {
-  const { loading: ethLoading, data: ethPriceData } = useQuery(ETH_PRICE_QUERY)
-  const { loading: daiLoading, data: daiData } = useQuery(DAI_QUERY, {
+  const {
+    loading: ethLoading,
+    data: ethPriceData
+  } = useQuery(ETH_PRICE_QUERY);
+
+  const {
+    loading: daiLoading,
+    data: daiData
+  } = useQuery(DAI_QUERY, {
     variables: {
       tokenAddress: '0x6b175474e89094c44da98b954eedeac495271d0f'
     }
-  })
+  });
 
-  const daiPriceInEth = daiData && daiData.tokens[0].derivedETH
-  const daiTotalLiquidity = daiData && daiData.tokens[0].totalLiquidity
-  const ethPriceInUSD = ethPriceData && ethPriceData.bundles[0].ethPrice
+  // TODO: use optional chaining
+  const daiPriceInEth = daiData && daiData.tokens[0].derivedETH;
+  const daiTotalLiquidity = daiData && daiData.tokens[0].totalLiquidity;
+  const ethPriceInUSD = ethPriceData && ethPriceData.bundles[0].ethPrice;
 
   return (
     <div>
@@ -54,31 +64,42 @@ function App() {
           className="navbar-brand col-sm-3 col-md-2 mr-0"
           href="http://www.dappuniversity.com/bootcamp"
           target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src={uniswapLogo} width="30" height="30" className="d-inline-block align-top" alt="" />
+          rel="noopener noreferrer">
+          <img
+            src={uniswapLogo}
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+            alt="" />
           &nbsp; Uniswap Explorer
         </a>
       </nav>
       <div className="container-fluid mt-5">
         <div className="row">
-          <main role="main" className="col-lg-12 d-flex text-center">
+          <main
+            role="main"
+            className="col-lg-12 d-flex text-center">
             <div className="content mr-auto ml-auto">
               <div>
-                <img src={daiLogo} width="150" height="150" className="mb-4" alt="" />
+                <img
+                  src={daiLogo}
+                  width="150"
+                  height="150"
+                  className="mb-4"
+                  alt="" />
                 <h2>
                   Dai price:{' '}
                   {ethLoading || daiLoading
                     ? 'Loading token data...'
                     : '$' +
-                      // parse responses as floats and fix to 2 decimals
+                      // Parse responses as floats and fix to 2 decimals
                       (parseFloat(daiPriceInEth) * parseFloat(ethPriceInUSD)).toFixed(2)}
                 </h2>
                 <h2>
                   Dai total liquidity:{' '}
                   {daiLoading
                     ? 'Loading token data...'
-                    : // display the total amount of DAI spread across all pools
+                    : // Display the total amount of DAI spread across all pools
                       parseFloat(daiTotalLiquidity).toFixed(0)}
                 </h2>
               </div>
@@ -90,4 +111,8 @@ function App() {
   );
 }
 
-export default App
+export {
+  client
+};
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap.css';
+import { ApolloProvider } from 'react-apollo';
+
 import App from './components/App';
+import { client } from './components/App';
 import * as serviceWorker from './serviceWorker';
-import { ApolloProvider } from 'react-apollo'
-import { client } from './components/App'
 
 ReactDOM.render(
   <ApolloProvider client={client}>
     <App />
   </ApolloProvider>,
   document.getElementById('root')
-)
+);
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
