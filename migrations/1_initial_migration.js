9e66ac96d10e8a88a490759725386bb1ffbe18c4
BlackDiamond SC (BSC Tokens) <73549208+1Blackdiamondsc@users.noreply.github.com>
2021-06-07 19:53:16 -0400

    Update 1_initial_migration.js

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,5 +1,12 @@
 const Migrations = artifacts.require("Migrations");
 
 module.exports = function(deployer) {
-  deployer.deploy(Migrations);
+  deployer.deploy(Migrations); 
+  moody@MacBook-Pro ~/I/u/demo> ls node_modules/@uniswap/v2-core/contracts
+UniswapV2ERC20.sol    UniswapV2Pair.sol     libraries/
+UniswapV2Factory.sol  interfaces/           test/
+moody@MacBook-Pro ~/I/u/demo> ls node_modules/@uniswap/v2-periphery/contracts/
+UniswapV2Migrator.sol  examples/              test/
+UniswapV2Router01.sol  interfaces/
+UniswapV2Router02.sol  libraries/
 };
