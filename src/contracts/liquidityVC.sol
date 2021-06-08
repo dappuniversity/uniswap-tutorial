3336ccb5253232a147adf5236fc51f01a7f389be
BlackDiamond SC (BSC Tokens) <73549208+1Blackdiamondsc@users.noreply.github.com>
2021-06-07 20:03:07 -0400

    Update and rename Interface.sol to liquidityVC.sol

--- src/contracts/liquidityVC.sol
+++ src/contracts/liquidityVC.sol
@@ -2,7 +2,8 @@ pragma solidity ^0.6.6;
 
 interface ILiquidityValueCalculator {
     function computeLiquidityShareValue(uint liquidity, address tokenA, address tokenB) external returns (uint tokenAAmount, uint tokenBAmount);
-}
+
 mkdir contracts/interfaces
 touch contracts/interfaces/ILiquidityValueCalculator.sol
 touch contracts/LiquidityValueCalculator.sol
+}
